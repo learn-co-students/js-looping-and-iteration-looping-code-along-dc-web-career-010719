754c994d32c991ba44d9cc8004079f052c224e00
Anthony Gregg <agregg22@gmail.com>
2019-02-17 17:31:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  }
+
+  return names;
+}
+
+function tailsNeverFails() {
+  flip = 0;
+  while (Math.random() >= 0.5) {
+    ++flip;
+  }
+  return `You got ${flip} tails in a row!`;
+}
