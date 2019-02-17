d729fef4b0c7fbc03fa0353c577c14dbd12a57ca
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-02-17 09:15:33 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function printBadges(badges) {
+  for ( let i = 0; i < badges.length; i++ ) {
+    console.log(`Welcome ${badges[i]}! You are employee #${i+1}.`);
+  };
+  return badges;
+};
+
+function tailsNeverFails() {
+  function maybetrue() {
+    return Math.random() >= 0.5;
+  };
+  let i=0;
+  while (maybetrue()) {
+    i++;
+  };
+  return `You got ${i} tails in a row!`
+};
